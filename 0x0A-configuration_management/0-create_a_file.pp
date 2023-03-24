5338ce50647af89b70dad9ea6e48970454216e36
AishaK <aishakhalfan45@gmail.com>
2023-03-24 18:37:39 +0000

    Task 0:0.Using Puppet Create a file in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file with specifications
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
